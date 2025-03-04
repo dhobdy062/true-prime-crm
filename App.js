64001ddd296c5eeb837e909397c0f15b68fe3e6b
Donchalant <56202953+dhobdy062@users.noreply.github.com>
2025-03-04 09:53:37 -0600

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TruePrimeCRM from './components/TruePrimeCRM';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <TruePrimeCRM />
+    </div>
+  );
+}
+
+export default App;
